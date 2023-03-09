9612e094dad41e716b63478315b02414645034d8
saladbowl77 <sarakun.info@gmail.com>
2023-03-10 00:31:24 +0900

    fix : get uuid api

--- src/main/java/work/saladbowl/disgot/spigot/whitelist.java
+++ src/main/java/work/saladbowl/disgot/spigot/whitelist.java
@@ -68,14 +68,20 @@ public class whitelist {
         }
     }
 
-
-
     public static String getUUID(String name) {
         String uuid = "";
+
         try {
+            JsonObject resJson;
+
             BufferedReader in = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/users/profiles/minecraft/" + name).openStream()));
-            uuid = (((JsonObject)new JsonParser().parse(in)).get("id")).toString().replaceAll("\"", "");
-            uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+            resJson = new JsonParser().parse(in).getAsJsonObject();
+
+            uuid = resJson.get("id")
+                    .toString()
+                    .replaceAll("\"", "")
+                    .replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+
             in.close();
         } catch (Exception e) {
             uuid = "error";
